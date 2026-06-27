378741528e2b0d0c60c3f205e337fc776d68b11c
Yanshu Wang <wangyenshu@outlook.com>
2026-06-27 12:56:36 +0800

    update

---
+++ CoCoA-worker.js
@@ -0,0 +1,17 @@
+importScripts("https://cdn.jsdelivr.net/npm/xterm-pty@0.9.4/workerTools.js");
+
+onmessage = (msg) => {
+  self.Module = self.Module || {};
+  self.Module.preRun = self.Module.preRun || [];
+  
+  self.Module.ENV = { "LANG": "C", "LC_ALL": "C" };
+
+  self.Module.preRun.push(() => {
+    try { FS.mkdirTree('/src/CoCoA-5/bin'); } catch(e) {}
+    FS.chdir('/src/CoCoA-5'); 
+  });
+
+  importScripts("CoCoAInterpreter.js");
+
+  emscriptenHack(new TtyClient(msg.data));
+};
